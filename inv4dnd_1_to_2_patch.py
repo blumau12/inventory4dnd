0f51e1aae2e94d1de0e95701fe61f364c69b87c0
David Tsaryov <dtsaryov@zuzex.com>
2019-05-09 10:36:56 +0300

    just merge it

---
+++ inv4dnd_1_to_2_patch.py
@@ -0,0 +1,18 @@
+from inv4dnd_classes import *
+
+old_characters = []
+new_characters = []
+
+open_all(old_characters)
+
+
+for old_character in old_characters:  # в чарах
+    new_character = Character(old_character.name)
+    new_characters.append(new_character)
+
+    for category in old_character.container:  # в категориях
+        for item in old_character.container[category]:  # в предметах
+            new_character.add_item(Item(item.category, item.name, item.cost, item.weight))
+
+
+save_all(new_characters)
